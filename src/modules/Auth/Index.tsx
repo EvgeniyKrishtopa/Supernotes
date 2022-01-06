dfeebd78dc2d4434a02cf6f102c33fd0a3f386e7
Eugene Krishtopa <evgenykrishtopa@gmail.com>
2022-01-06 17:23:04 +0200

    create environment
    fix
    
    fix
    
    add changes
    
    feature
    
    create environment
    
    2
    
    create environment
    
    2
    
    test
    
    create environment
    
    create environment

---
+++ src/modules/Auth/Index.tsx
@@ -0,0 +1,14 @@
+import Header from 'components/Header/Index';
+import React from 'react';
+//import Header from '../../components/Header/Index';
+
+const Auth = () => {
+  return (
+    <div>
+      <Header />
+      123333
+    </div>
+  );
+};
+
+export default Auth;
